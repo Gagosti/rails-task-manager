41ef25c7a08bb5e8f9d590ad3aae87b0e15ffe3c
Guilherme Santos <agustinho_santos@hotmail.com>
2022-05-24 16:51:17 -0300

    atividade finalizada

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new'
+  # post "tasks", to: "tasks#create"
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch "tasks/:id", to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
+
+  resources :tasks
 end
